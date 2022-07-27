f6c16e078d6a94cec46a6d34f58b5a98c2587b5f
Dheeraj Kumar Jha <jhadheeraj268@gmail.com>
2022-07-27 20:12:53 +0530

    db connection and operation

--- Application/settings.py
+++ Application/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'Application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python',
+        'USER': 'root',
+        'PASSWORD': '268@Gmail',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+
     }
 }
 
--- app1/views.py
+++ app1/views.py
@@ -1,13 +1,19 @@
-from django.http import HttpResponse
+from django.db import connection
 from django.shortcuts import render
 
 # Create your views here.
 def xyz(request):
     return render(request,"index.html")
 def signUp(request):
-    print("signup method is working")
-    i=5
-    i=i+7
-    j=i
-    j=j+i
-    return render(request,"index.html")
+    email=request.GET['email']
+    psw=request.GET['psw']
+    cursor=connection.cursor()
+    query="insert into users (email, password) values (%s, %s)"
+    # query = "select * from city where name= '" + email + "'"
+    value=(email,psw)
+    cursor.execute(query,value)
+    print(cursor.rowcount)
+    # row=cursor.fetchone()
+    # print(row)
+    data={"email":email,"password":psw}
+    return render(request,"first.html",data)
